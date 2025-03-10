be1e50e12bf2aa3692abfac19987e63510fe17ab
Rolando Martins <rolando.martins@gmail.com>
2025-03-10 22:23:35 +0000

    re-adding semantichunker

--- backend/open_webui/routers/retrieval.py
+++ backend/open_webui/routers/retrieval.py
@@ -26,6 +26,8 @@ from pydantic import BaseModel
 import tiktoken
 
 
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_ollama import OllamaEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter, TokenTextSplitter
 from langchain_core.documents import Document
 
@@ -798,11 +800,13 @@ def save_docs_to_vector_db(
 
     if split:
         if request.app.state.config.TEXT_SPLITTER in ["", "character"]:
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=request.app.state.config.CHUNK_SIZE,
-                chunk_overlap=request.app.state.config.CHUNK_OVERLAP,
-                add_start_index=True,
-            )
+            embeddings = OllamaEmbeddings(model='avr/sfr-embedding-mistral:f16', base_url="http://127.0.0.1:11434")
+            text_splitter = SemanticChunker(embeddings, add_start_index=True, breakpoint_threshold_type="gradient")
+            #text_splitter = RecursiveCharacterTextSplitter(
+            #    chunk_size=request.app.state.config.CHUNK_SIZE,
+            #    chunk_overlap=request.app.state.config.CHUNK_OVERLAP,
+            #    add_start_index=True,
+            #)
         elif request.app.state.config.TEXT_SPLITTER == "token":
             log.info(
                 f"Using token text splitter: {request.app.state.config.TIKTOKEN_ENCODING_NAME}"
